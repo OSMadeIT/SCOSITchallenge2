4123d99b6f3755a74c1412420460b6452e83b858
Stephen Oyaro <vystoya@gmail.com>
2016-11-19 13:47:51 +0300

    grammar and comment positioning
    The second comment wasn't flowing in terms of grammar. And I put it beside the 'count' that it is commenting on.
    Otherwise, great logic!

--- pius_dan.py
+++ pius_dan.py
@@ -1,8 +1,6 @@
 n = [10, 20, 20, 10, 10, 30, 50, 10, 20]
 count = 0
 for i in set(n):    # convert the list n to a set and loop over each value of the set
-    # count inbuilt function to count the number of occurance of a value in an
-    # list
-    if n.count(i) // 2 >= 1:
+    if n.count(i) // 2 >= 1:     # count is an inbuilt function that counts the number of occurance of a value in a list
         count += n.count(i) // 2
 print(count)
